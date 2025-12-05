7b41f4fa84159bb336f738b0eb7f3db5b7d3b127
pastorini998 <pastorini.ifts@gmail.com>
2025-12-05 02:23:54 -0300

    click

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,10 +9,11 @@ class LoginPage(BasePage):
     ERROR_MESSAGE = (By.CSS_SELECTOR, "h3[data-test='error']")
 
     # Acciones
-    def login(self, username, password):
+    def click_login(self, username, password):
         self.enter_text(self.USERNAME_INPUT, username)
         self.enter_text(self.PASSWORD_INPUT, password)
         self.click(self.LOGIN_BUTTON)
+        self.driver.find_element(*self._LOGIN_BUTTON).click()
     
     def get_error_message(self):
         return self.get_text(self.ERROR_MESSAGE)
