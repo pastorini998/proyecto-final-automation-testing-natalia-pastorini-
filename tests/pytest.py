d7ec66b0baa08160b3cb22dc5b7d07f98d05cc4f
pastorini998 <pastorini.ifts@gmail.com>
2025-12-05 21:29:33 -0300

    pytest

--- tests/pytest.py
+++ tests/pytest.py
@@ -7,4 +7,77 @@ def driver():
     driver = get_driver()
     yield driver
     driver.quit()
-    
+    import pytest
+import logging
+import pathlib
+import time
+from selenium import webdriver
+from pytest_html import extras
+
+
+
+@pytest.fixture
+def api_url():
+    return 'https://jsonplaceholder.typicode.com/'
+
+
+
+# def pytest_html_summers(prefix):
+
+#     prefix.extend([
+#         '<h2>MISION IM CUMPLIDA</h2>',
+#         '<div style="background:gold"></div>'
+#     ])
+
+
+def pytest_html_report_title(report):
+    """Cambia el título de la pestaña del navegador"""
+    report.title = "TalentoLab – Reporte de Testing"
+
+path_dir = pathlib.Path('logs')
+path_dir.mkdir(exist_ok=True)
+
+
+logging.basicConfig(
+    filename= path_dir/ "historial.log",
+    level= logging.INFO,
+    format='%(asctime)s %(levelname)s %(name)s – %(message)s',
+    datefmt='%H:%M:%S'
+)
+
+logger = logging.getLogger()
+
+# 1. Carpeta para screenshots
+target = pathlib.Path('reports/screens')
+target.mkdir(parents=True, exist_ok=True)
+
+# 2. Fixture simple
+@pytest.fixture
+def driver():
+    """Fixture para Chrome"""
+    d = webdriver.Chrome()
+    d.get("https://google.com")
+    time.sleep(2)  # Espera a que cargue
+    yield d
+    d.quit()
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == 'call' and report.failed:
+        if 'driver' in item.fixturenames:
+            driver = item.funcargs['driver']
+
+            file_name = target / f"{item.name}.png"
+            driver.save_screenshot(str(file_name))
+
+            report.extra = getattr(report, 'extra', [])
+
+            report.extra.append(extras.png(str(file_name)))
+
+def pytest_configure(config):
+    config.addinivalue_line(
+        "markers", "env(name): mark test to run only on named environment"
+    )           
